d9956d4945833365be19f48983e343ee226ded95
Kamil Can Engin <kamilcanengin@obdansistem.com>
2024-06-11 17:43:34 +0300

    GetProductsWithCategory ops.

--- NLayer.Core/DTOs/ProductUpdateDto.cs
+++ NLayer.Core/DTOs/ProductUpdateDto.cs
@@ -12,6 +12,7 @@ namespace NLayer.Core.DTOs
         public string? Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
+        public int CategoryId { get; set; }
 
     }
 }
---
+++ NLayer.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+
+        Task<List<Product>> GetProductsWithCategory();
+
+    }
+}
